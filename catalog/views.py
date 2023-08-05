b21fd18075436339b0552566ea2fe8ea850e414f
Alexandr Noskov <san9nosko8@gmail.com>
2023-08-05 17:14:26 +0300

    Контролер

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request) -> HttpResponse:
+    '''
+    Контролер для главной страницы интернет-магазина
+    :return: HTTP-ответ с отображением шаблона "home.html"
+    '''
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request) -> HttpResponse:
+    '''
+    Контролер для клиента интернет-магазина
+    :return: HTTP-ответ с отображением шаблона "contacts.html"
+    '''
+    if request.method == 'POST':
+        data = {}
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        data['user_1'] = (name, email, message)
+        print(data)
+
+    return render(request, 'catalog/contacts.html')
