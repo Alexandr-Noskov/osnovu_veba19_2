d755f17de876d9969442f4eb3d0c80725a92e13c
Alexandr Noskov <san9nosko8@gmail.com>
2023-08-23 01:15:28 +0300

    new ur

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
+
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog.views import home, contacts, category_product
 
 urlpatterns = [
     path('', home),
-    path('contacts/', contacts)
+    path('contacts/', contacts),
+    path('category_product/', category_product),
+    path('contacts/', contacts, name="contacts"),
+
 ]
--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from catalog.views import contacts
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls'))
+    path('', include('catalog.urls', namespace='catalog')),
+    path('contacts/', contacts, name="contacts")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
