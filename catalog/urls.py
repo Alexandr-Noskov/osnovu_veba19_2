ba379dfacb591e4b9a29e1c8492acdb953aee2fa
Alexandr Noskov <san9nosko8@gmail.com>
2023-08-23 20:27:25 +0300

    new ur

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,17 @@
+from itertools import product
 
 from django.urls import path
 
 from catalog.views import home, contacts, category_product
 
+
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
+
 urlpatterns = [
     path('', home),
-    path('contacts/', contacts),
-    path('category_product/', category_product),
     path('contacts/', contacts, name="contacts"),
+    path('category_product/', category_product, name="category_product"),
 
 ]
