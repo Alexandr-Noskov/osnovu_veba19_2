bb8b0712e7f8960c96bc87d496b101ee2de9fea9
Alexandr Noskov <san9nosko8@gmail.com>
2023-08-30 20:19:34 +0300

    ispravlenie3

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from itertools import product
 
 from django.urls import path
 
-from catalog.views import home, contacts, category_product, create_product, categories, product
+from catalog.views import home, contacts, category_product, create_product, categories, product, products
 
 from catalog.apps import CatalogConfig
 
@@ -15,6 +15,6 @@ urlpatterns = [
     path('category_product/<int:pk>', category_product, name="category_product"),
     path('create_product/', create_product, name='create_product'),
     path('product/<int:pk>', product, name='product'),
-    path('products/<int:pk>', product, name='products'),
+    path('products/', products, name='products'),
 
 ]
