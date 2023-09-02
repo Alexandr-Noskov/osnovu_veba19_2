b2f031addf01a09986e0413597c1bd0bd2c6f14e
Alexandr Noskov <san9nosko8@gmail.com>
2023-09-02 16:59:48 +0300

    ispravlenie4

--- catalog/urls.py
+++ catalog/urls.py
@@ -18,7 +18,6 @@ urlpatterns = [
     path('categories/', categories, name='categories'),
     path('category_product/<int:pk>', category_product, name="category_product"),
     path('create_product/', create_product, name='create_product'),
-    path('product/<int:pk>', product, name='product'),
     path('product/<int:pk>/', product, name='product'),
     path('product/view/<int:pk>', ProductDetailView.as_view(), name='product_view'),
     path('product/edit/<int:pk>', ProductUpdateView.as_view(), name='product_edit'),
